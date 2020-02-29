cff9733741d8f5f7264f8e20ab325f75de06414e
Stephen Anderson <steppyandrews@gmail.com>
2020-02-29 18:28:19 +0000

    The solution now contains the test project - currently doesn't run any tests

---
+++ SDI-Master/Unit-Tests/Unit-Tests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../SDI-Master/MainWindow.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::string fileName = "treez.jpg";
+			
+		}
+	};
+}
